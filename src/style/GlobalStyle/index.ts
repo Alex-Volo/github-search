053fcb2084a0a8960fb4cc2a79ea6356e1d3ec40
Alex Volo <alex_volo@bk.ru>
2024-03-30 00:12:11 +0900

    initial

---
+++ src/style/GlobalStyle/index.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Roboto';
+    font-weight: normal;
+    font-display: swap;
+    src: '/assets/fonts/Roboto-Regular.ttf';
+  }
+
+  :root {
+    
+  }
+  
+  body {
+    margin: 0;
+    font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #383838;
+    min-height: 100vh;
+    min-width: 100%;
+    max-width: 100%;
+  }
+  
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 100vw;
+    min-height: 100vh;
+  }
+`
