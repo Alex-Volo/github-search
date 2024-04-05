31b72bff3af69022563a88c88e2156738b65131e
Alex Volo <alex_volo@bk.ru>
2024-04-05 16:23:41 +0900

    hz

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,17 +4,20 @@ import type { IResponseUserDetailed, Order, Page, SortBy } from 'types'
 export const searchUser = async (q: string, sort?: SortBy, order?: Order, page?: Page) => {
   try {
     const octokit = new Octokit()
-
-    return await octokit.request('GET /search/users', {
+    const response = await octokit.request('GET /search/users', {
       q,
       sort,
       order,
       page,
       'X-GitHub-Api-Version': '2022-11-28'
     })
+
+    if (!response) throw new Error('Нет сети')
+
+    return response
   } catch (error) {
     if (error instanceof Error) {
-      console.warn(error.message)
+      throw new TypeError(error.message)
     }
   }
 }
@@ -25,10 +28,11 @@ export const getUser = async (username: string): Promise<IResponseUserDetailed |
     const response = (await octokit.request(`GET /users/${username}`, {
       'X-GitHub-Api-Version': '2022-11-28'
     })) as unknown
+    if (!response) throw new Error('Нет сети')
     return response as IResponseUserDetailed
   } catch (error) {
     if (error instanceof Error) {
-      console.warn(error)
+      throw new TypeError(error.message)
     }
   }
 }
